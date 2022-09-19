47afd2798c6e904a90ec87cc3f71239dd859a1aa
geraldc <cham.gerald@ait.ac.at>
2022-09-19 11:12:18 +0200

    Function: Print string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+    void _puts(char *str);
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
+}
+
+void _puts(char *str){
+	while(*str != '\0')
+        printf("%c",*str++);
+}
