317af5e44308c3ac45cfd8ba172dc23802e32dd9
Gerald Cham Kpu <66692375+JerryWiseman@users.noreply.github.com>
2022-09-20 22:35:15 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,17 +4,8 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void _puts(char *str)
 {
-    char *str;
-    void _puts(char *str);
-
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
-}
-
-void _puts(char *str){
 	while(*str != '\0')
         	printf("%c",*str++);
 	printf("\n");
