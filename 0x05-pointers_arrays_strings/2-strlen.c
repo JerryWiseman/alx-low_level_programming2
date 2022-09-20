05dc35446f23b5586d25e75573817499093f85cf
Gerald Cham Kpu <66692375+JerryWiseman@users.noreply.github.com>
2022-09-20 22:12:05 +0200

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,13 +6,3 @@ int _strlen(char *s){
     return (i);
     } 
     
-int main(void)
-{
-    char *str;
-    int len;
-    
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
-}
