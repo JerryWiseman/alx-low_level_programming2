cc2511cadd1ad56f936447bb9f9e56b8bd6b2b49
Gerald Cham Kpu <66692375+JerryWiseman@users.noreply.github.com>
2022-09-20 22:43:37 +0200

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,19 +4,8 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void puts2(char *str)
 {
-    void puts2(char *str);
-
-    char *str;
-
-    str = "0123456789";
-    puts2(str);
-    return (0);
-}
-
-
-void puts2(char *str){
 	while(*str != '\0')
         	printf("%c",*str++);
 	printf("%c\n");
