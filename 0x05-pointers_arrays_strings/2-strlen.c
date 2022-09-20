4b872341c3f1fe1469200b5b399905f58dc725f5
Gerald Cham Kpu <66692375+JerryWiseman@users.noreply.github.com>
2022-09-20 22:22:38 +0200

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-int _strlen(char *s){
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ *
+int _strlen(char *s)
+{
 	int i;
 	for (i = 0; s[i] != '\0'; ++i);
-    return (i);
-    } 
-    
+	return (i);
+} 
