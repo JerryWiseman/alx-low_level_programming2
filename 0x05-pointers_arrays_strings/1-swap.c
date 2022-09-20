ee5571042930b61794ca3117245b7bcc1ce563cd
Gerald Cham Kpu <66692375+JerryWiseman@users.noreply.github.com>
2022-09-20 22:34:34 +0200

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,22 +5,9 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void swap_int(int *a, int *b)
 {
-    int a;
-    int b;
-    void swap_int(int *a, int *b);
-
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
-}
-
-void swap_int(int *a, int *b){
 	int temp = *a;
 	*a = *b;
-    *b = temp;
+    	*b = temp;
 }
