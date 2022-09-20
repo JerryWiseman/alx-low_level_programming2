bf60ced97ce1224078bdf0ecc9adec6b5f83fc30
Gerald Cham Kpu <66692375+JerryWiseman@users.noreply.github.com>
2022-09-20 22:33:40 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,19 +5,6 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    int n;
-    void reset_to_98(int *n);
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
-    
-}
-
 void reset_to_98(int *n)
 {
   *n = 98;
