8e65875e318811c18ea748c19e1df96202ed5396
Gerald Cham Kpu <66692375+JerryWiseman@users.noreply.github.com>
2022-09-20 22:36:55 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,40 +4,28 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int _strlen(char *s)
 {
-    char *str;
-    void print_rev(char *s);
-    int _strlen(char *s);
-
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    print_rev(str);
-    return (0);
-}
-
-
-int _strlen(char *s){
 	int i;
 	for (i = 0; s[i] != '\0'; ++i);
-    return (i);
+    	return (i);
 }
 
-
-void print_rev(char *s){
+void print_rev(char *s)
+{
 	int l, i;
-  char *end_ptr;
+  	char *end_ptr;
  
-  l = _strlen(s);
- 
-  end_ptr = s;
+  	l = _strlen(s);
+  	end_ptr = s;
  
   // Move the end_ptr to the last character
   for (i = 0; i < l - 1; i++)
     end_ptr++;
 
-  for (i = 0; i < l; i++) {
+  for (i = 0; i < l; i++)
+  {
     printf("%c",*end_ptr);
     end_ptr--;
   }
- 
 }
