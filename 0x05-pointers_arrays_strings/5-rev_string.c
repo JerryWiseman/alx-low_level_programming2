25c05b682230529e8b94fefe9c9a207f80a277c7
Gerald Cham Kpu <66692375+JerryWiseman@users.noreply.github.com>
2022-09-20 22:42:36 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,47 +4,29 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int _strlen(char s[])
 {
-    void rev_string(char *s);
-    int _strlen(char s[]);
-
-     char s[10] = "My School";
-
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
-    return (0);
-}
-
-
-int _strlen(char s[]){
 	int i;
 	for (i = 0; s[i] != '\0'; ++i);
-    return (i);
+    	return (i);
 }
 
-
-void rev_string(char *s){
+void rev_string(char *s)
+{
 	int l, i;
-  char *end_ptr, *begin_ptr;
+  	char *end_ptr, *begin_ptr;
  
-  l = _strlen(s);
- 
-  end_ptr = s;
-  begin_ptr = s;
- 
-  // Move the end_ptr to the last character
-  for (i = 0; i < l-1; i++)
-    end_ptr++;
-
-  for (i = 0; i < l/2; i++) {
-    char temp = *end_ptr;
-    *end_ptr = *begin_ptr;
-    *begin_ptr = temp;
-
-    end_ptr--;
-    begin_ptr++;
-  }
- 
+  	l = _strlen(s);
+  	end_ptr = s;
+  	begin_ptr = s;
+  	for (i = 0; i < l-1; i++)
+    		end_ptr++;
+  	for (i = 0; i < l/2; i++)
+	{
+    		char temp = *end_ptr;
+    		*end_ptr = *begin_ptr;
+    		*begin_ptr = temp;
+    		end_ptr--;
+    		begin_ptr++;
+  	}
 }
