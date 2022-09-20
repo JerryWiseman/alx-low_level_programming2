c901d074a2c174705932dc0a685f336fa555420b
Gerald Cham Kpu <66692375+JerryWiseman@users.noreply.github.com>
2022-09-20 22:52:19 +0200

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,23 +4,12 @@
  *
  * Return: Always 0.
  */
-int main(void)
+char *_strcpy(char *dest, char *src)
 {
-    char s1[98];
-    char *ptr;
-    char *_strcpy(char *dest, char *src);
-
-    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-    printf("%s", s1);
-    printf("%s", ptr);
-    return (0);
-}
-
-
-char *_strcpy(char *dest, char *src){
     char *begin_dest = dest;
-    do{
+    do
+    {
         *dest++ = *src++;
-    }while(*src != '\0');
-	return begin_dest;
+    } while (*src != '\0');
+    return begin_dest;
 }
